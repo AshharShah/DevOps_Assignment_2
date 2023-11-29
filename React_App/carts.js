3c70e8872de736b896bb97e76d90e8589326b5ba
AshharShah <ashhar.shah786@gmail.com>
2023-11-29 13:43:09 +0500

    Changed Item Name

--- React_App/carts.js
+++ React_App/carts.js
@@ -6,13 +6,13 @@ let activeCarts = [
     cartID: "777",
     cartItems: [
       {
-        title: "Strawberries",
+        title: "Fresh Strawberries",
         description: "Assorted sizes, 2 cartons",
         cost: 200,
         imageUrl: "stawberries.jpg"
       },
       {
-        title: "Blackberries",
+        title: "Fresh Blackberries",
         description: "Assorted sizes, 2 cartons",
         cost: 295,
         imageUrl: "blackberries.jpg"
